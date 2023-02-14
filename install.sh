26caed367e28a7cf7c50ece7cb3478f014cc7509
jbroberg42 <ninjonah1@gmail.com>
2023-02-13 23:53:08 -0600

    added the option to overwrite files during installation

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo 'running install script...'
+echo -e "running install script...\n"
 
-cat ./.vimrc >> ~/.vimrc
-cat ./.tmux.conf >> ~/.tmux.conf
-cat ./.bash_aliases >> ~/.bash_aliases && source ~/.bash_aliases
 
-echo 'script completed.'
+if [ "$1" == "-o" ]; then
+    echo -e "overwrite option selected\n"
+    cat ./.vimrc > ~/.vimrc
+    cat ./.tmux.conf > ~/.tmux.conf
+    cat ./.bash_aliases > ~/.bash_aliases && source ~/.bash_aliases
+elif [ "$1" == "-a" ]; then
+    cat ./.vimrc >> ~/.vimrc
+    cat ./.tmux.conf >> ~/.tmux.conf
+    cat ./.bash_aliases >> ~/.bash_aliases && source ~/.bash_aliases
+else
+    echo -e "usage: must specify an argument\n-o -- overwrite current configs\n-a -- add new lines to the configs\n"
+fi
+
+echo -e "script completed.\n"
